b88e65baed737eac5a146eb10e98a0003de5cbbf
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-05-29 12:01:05 +0900

    70.ルーティングの作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,7 +2,8 @@ import { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
-import { HomeRoutes } from "./homeRoutes";
+import { homeRoutes } from "./HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
 
 export const Router: VFC = memo(() => {
   return (
@@ -26,6 +27,9 @@ export const Router: VFC = memo(() => {
           </Switch>
         )}
       />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 });
